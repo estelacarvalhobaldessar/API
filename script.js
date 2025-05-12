94b55e33ab8d28793684616f1ea45032daef6d52
Estela de C. Baldessar <ecbaldessar@gmail.com>
2025-05-12 09:12:23 -0300

    api

--- script.js
+++ script.js
@@ -1,21 +1,11 @@
-'use strict'
- 
-async function loadGallery() {
-    const gallery = document.getElementById('gallery')
-    const url = 'https://api.thecatapi.com/v1/images/search?limit=10%27'
- 
-    const response = await fetch(url)
-    const cats = await response.json()
- 
-    // Limpa a galeria antes de adicionar novas imagens
-    gallery.innerHTML = '';
- 
-    // Adiciona as novas imagens de gatos
-    cats.forEach(cat => {
-        const img = document.createElement('img')
-        img.src = cat.url
-        gallery.appendChild(img)
-    })
-    console.log(cats)
+const dogImage = document.getElementById('dog-image')
+
+ async function updateDogImage(){
+    const url = 'https://dog.ceo/api/breeds/image/random';
+    const response = await fetch (url);
+    const data = await response.json();
+
+    dogImage.src = data.message; 
 }
-loadGallery();
+
+dogImage.addEventListener('click', updateDogImage);
